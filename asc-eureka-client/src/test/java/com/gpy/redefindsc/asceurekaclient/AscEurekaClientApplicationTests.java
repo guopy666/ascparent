f478a6575be747c728c3489b1eb9214bc08d6362
guopengying <1315977102@qq.com>
2020-07-23 17:28:28 +0800

    Initial commit

---
+++ asc-eureka-client/src/test/java/com/gpy/redefindsc/asceurekaclient/AscEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpy.redefindsc.asceurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscEurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ asc-eureka-server/src/test/java/com/gpy/redefindsc/asceurekaserver/AscEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpy.redefindsc.asceurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
