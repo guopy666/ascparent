0479b05237b726f8d6e75835be08d68baf22f03d
guopengying <1315977102@qq.com>
2020-07-24 09:43:36 +0800

    eureka config server init

---
+++ asc-config-server/src/main/java/com/gpy/redefindsc/ascconfigserver/AscConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.gpy.redefindsc.ascconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AscConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AscConfigServerApplication.class, args);
+    }
+
+}
