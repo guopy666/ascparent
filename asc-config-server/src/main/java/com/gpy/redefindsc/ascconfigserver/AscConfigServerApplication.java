35a2a0a682907cfb66636d0b726cf961ed243f84
guopengying <1315977102@qq.com>
2020-07-24 17:48:32 +0800

    feign upload file

--- asc-config-server/src/main/java/com/gpy/redefindsc/ascconfigserver/AscConfigServerApplication.java
+++ asc-config-server/src/main/java/com/gpy/redefindsc/ascconfigserver/AscConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.gpy.redefindsc.ascconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class AscConfigServerApplication {
 
     public static void main(String[] args) {
